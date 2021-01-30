0d359e0929bf3b41e54837b486bcc6442b0854c0
neinomas <neinomas@gmail.com>
2021-01-30 11:53:25 +0000

    updated routes with methods

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,14 @@ from flask import render_template, redirect, url_for, request
 from app import app, db
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     return render_template('register.html')
