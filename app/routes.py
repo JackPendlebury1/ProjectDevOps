9db428095582fc871872ba06c3b2c254c6a85630
neinomas <neinomas@gmail.com>
2021-01-30 11:41:10 +0000

    added routes.py added login route add index route added register route

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from flask import render_template, redirect, url_for, request
+from app import app, db
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
