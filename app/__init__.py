d1761165f9f356ae84d59c9500e62be7fb067173
neinomas <neinomas@gmail.com>
2021-02-01 15:12:38 +0000

    added bcrpyt to the app

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from decouple import config
 import os
-
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__,template_folder='../templates')
+bcrypt = Bcrypt(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE')
+app.config['SECRET_KEY'] = os.environ.get('SECRET')
 
 db = SQLAlchemy(app)
 
