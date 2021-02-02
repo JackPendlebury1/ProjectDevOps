ae07b094a694f25af3bc4be6d3e8d8befd6e019e
neinomas <neinomas@gmail.com>
2021-02-02 13:14:53 +0000

    added login manager to init pointed to login view

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__,template_folder='../templates')
 bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE')
 app.config['SECRET_KEY'] = os.environ.get('SECRET')
-
-db = SQLAlchemy(app)
 
 from app import routes
