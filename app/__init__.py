8ee1932e98d441e5f678e0c5b5008b7eec02ea66
neinomas <neinomas@gmail.com>
2021-02-02 14:34:38 +0000

    removed debug from app.run

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from os import getenv
 
 app = Flask(__name__,template_folder='../templates')
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE')
+app.config['SECRET_KEY'] = getenv('SECRET')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE')
-app.config['SECRET_KEY'] = os.environ.get('SECRET')
 
 from app import routes
