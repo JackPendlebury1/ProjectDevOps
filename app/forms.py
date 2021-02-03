b0d5efbee0717eb40f83f9fc2b5930bae8b8f8ca
neinomas <neinomas@gmail.com>
2021-02-03 13:33:32 +0000

    add update user and song forms

--- app/forms.py
+++ app/forms.py
@@ -18,7 +18,6 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self, email):
             user = Users.query.filter_by(user_email=email.data).first()
-
             if user:
                 raise ValidationError('This email is already registered do you have an account?')
 
@@ -32,3 +31,26 @@ class SongForm(FlaskForm):
     length = IntegerField('Song Length', validators=[DataRequired()])
 
     submit = SubmitField('Add Song')
+
+class SongUpdateForm(FlaskForm):
+    name = StringField('Song Name', validators=[DataRequired(), Length(2,50)])
+    artist = StringField('Artist', validators=[DataRequired(), Length(2,50)])
+    genre = StringField('Genre', validators=[DataRequired(), Length(2,50)])
+    release_date = StringField('Release Date', validators=[DataRequired()])
+    length = IntegerField('Song Length', validators=[DataRequired()])
+
+    submit = SubmitField('Add Song')
+
+class UserUpdateForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(2,30)])
+    password = StringField('Password', validators=[DataRequired(), Length(8,32)])
+    first_name = StringField('First Name', validators=[DataRequired(), Length(1,20)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(1,50)])
+    email = StringField('Email', validators=[DataRequired()])
+
+    def validate_email(self, email):
+            user = Users.query.filter_by(user_email=email.data).first()
+            if user:
+                raise ValidationError('This email is already registered do you have an account?')
+
+    submit = SubmitField('Register')
